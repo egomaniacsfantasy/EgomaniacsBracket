936125f8ceba085c58203e35b2c41c347e28be9b
Andre Vlahakis <andrevlahakis@Andres-MacBook-Pro.local>
2026-03-17 23:58:32 -0400

    Lock Texas First Four result

--- src/data/ncaaKnownResults.ts
+++ src/data/ncaaKnownResults.ts
@@ -6,6 +6,7 @@ import type { LockedPicks } from "../lib/bracket";
  */
 export const NCAA_KNOWN_RESULTS: LockedPicks = {
   "Midwest-FF-16": "Midwest-16b", // Howard (16b) def UMBC (16a)
+  "West-FF-11": "West-11b", // Texas (11b) def NC State (11a)
 };
 
 export const NCAA_KNOWN_RESULT_IDS = new Set(Object.keys(NCAA_KNOWN_RESULTS));
