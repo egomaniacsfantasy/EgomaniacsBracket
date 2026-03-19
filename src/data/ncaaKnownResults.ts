1c04001dc77bc30a3175f7bea2a3318e08cccf76
Andre Vlahakis <andrevlahakis@Andres-MacBook-Pro.local>
2026-03-19 14:29:10 -0400

    Lock TCU over Ohio State

--- src/data/ncaaKnownResults.ts
+++ src/data/ncaaKnownResults.ts
@@ -9,6 +9,7 @@ export const NCAA_KNOWN_RESULTS: LockedPicks = {
   "Midwest-FF-11": "Midwest-11a", // Miami OH (11a) def SMU (11b)
   "Midwest-FF-16": "Midwest-16b", // Howard (16b) def UMBC (16a)
   "West-FF-11": "West-11b", // Texas (11b) def NC State (11a)
+  "East-R64-1": "East-9", // TCU (9) def Ohio St (8)
 };
 
 export const NCAA_KNOWN_RESULT_IDS = new Set(Object.keys(NCAA_KNOWN_RESULTS));
